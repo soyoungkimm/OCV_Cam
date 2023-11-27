3b78d0c648e28b0395cb990aed23065cc132d1e1
soyoungkimm <ksoyoung09@gmail.com>
2023-11-27 13:07:14 +0900

    웹캠출력, 동영상 저장 및 실행 기능 구현

---
+++ Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace OCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Image1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.button_Record = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button_Close = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel_Name = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel_width = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel_Time = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel_etcname = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel_Etc = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel8 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button_Capture = new System.Windows.Forms.Button();
+            this.button_Play = new System.Windows.Forms.Button();
+            this.button_Open2 = new System.Windows.Forms.Button();
+            this.Image2 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.button_Face = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Image1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Image2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Image1
+            // 
+            this.Image1.BackColor = System.Drawing.Color.White;
+            this.Image1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Image1.Location = new System.Drawing.Point(4, 49);
+            this.Image1.Name = "Image1";
+            this.Image1.Size = new System.Drawing.Size(768, 432);
+            this.Image1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Image1.TabIndex = 0;
+            this.Image1.TabStop = false;
+            // 
+            // button_Record
+            // 
+            this.button_Record.ImageIndex = 1;
+            this.button_Record.ImageList = this.imageList1;
+            this.button_Record.Location = new System.Drawing.Point(567, 7);
+            this.button_Record.Name = "button_Record";
+            this.button_Record.Size = new System.Drawing.Size(38, 38);
+            this.button_Record.TabIndex = 22;
+            this.button_Record.UseVisualStyleBackColor = true;
+            this.button_Record.Click += new System.EventHandler(this.button_Record_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "camera.png");
+            this.imageList1.Images.SetKeyName(1, "record.png");
+            this.imageList1.Images.SetKeyName(2, "stop_red.png");
+            this.imageList1.Images.SetKeyName(3, "play.png");
+            this.imageList1.Images.SetKeyName(4, "stop_black.png");
+            this.imageList1.Images.SetKeyName(5, "face_on.png");
+            this.imageList1.Images.SetKeyName(6, "face_off.png");
+            this.imageList1.Images.SetKeyName(7, "icon_image2.png");
+            this.imageList1.Images.SetKeyName(8, "exit.png");
+            // 
+            // button_Close
+            // 
+            this.button_Close.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.button_Close.FlatAppearance.BorderSize = 0;
+            this.button_Close.ImageIndex = 8;
+            this.button_Close.ImageList = this.imageList1;
+            this.button_Close.Location = new System.Drawing.Point(733, 7);
+            this.button_Close.Name = "button_Close";
+            this.button_Close.Size = new System.Drawing.Size(38, 38);
+            this.button_Close.TabIndex = 20;
+            this.button_Close.UseVisualStyleBackColor = true;
+            this.button_Close.Click += new System.EventHandler(this.button_Close_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel_Name,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel_width,
+            this.toolStripStatusLabel5,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel_Time,
+            this.toolStripStatusLabel_etcname,
+            this.toolStripStatusLabel_Etc,
+            this.toolStripStatusLabel8});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 484);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(775, 24);
+            this.statusStrip1.TabIndex = 24;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(39, 19);
+            this.toolStripStatusLabel2.Text = "Name";
+            // 
+            // toolStripStatusLabel_Name
+            // 
+            this.toolStripStatusLabel_Name.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabel_Name.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.toolStripStatusLabel_Name.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatusLabel_Name.Name = "toolStripStatusLabel_Name";
+            this.toolStripStatusLabel_Name.Size = new System.Drawing.Size(63, 19);
+            this.toolStripStatusLabel_Name.Text = "             ";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(29, 19);
+            this.toolStripStatusLabel1.Text = "Size";
+            // 
+            // toolStripStatusLabel_width
+            // 
+            this.toolStripStatusLabel_width.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabel_width.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.toolStripStatusLabel_width.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatusLabel_width.Name = "toolStripStatusLabel_width";
+            this.toolStripStatusLabel_width.Size = new System.Drawing.Size(51, 19);
+            this.toolStripStatusLabel_width.Text = "          ";
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(0, 19);
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(33, 19);
+            this.toolStripStatusLabel4.Text = "Time";
+            // 
+            // toolStripStatusLabel_Time
+            // 
+            this.toolStripStatusLabel_Time.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabel_Time.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.toolStripStatusLabel_Time.Name = "toolStripStatusLabel_Time";
+            this.toolStripStatusLabel_Time.Size = new System.Drawing.Size(51, 19);
+            this.toolStripStatusLabel_Time.Text = "          ";
+            // 
+            // toolStripStatusLabel_etcname
+            // 
+            this.toolStripStatusLabel_etcname.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.toolStripStatusLabel_etcname.Name = "toolStripStatusLabel_etcname";
+            this.toolStripStatusLabel_etcname.Size = new System.Drawing.Size(35, 19);
+            this.toolStripStatusLabel_etcname.Text = " ETC ";
+            // 
+            // toolStripStatusLabel_Etc
+            // 
+            this.toolStripStatusLabel_Etc.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabel_Etc.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.toolStripStatusLabel_Etc.Name = "toolStripStatusLabel_Etc";
+            this.toolStripStatusLabel_Etc.Size = new System.Drawing.Size(51, 19);
+            this.toolStripStatusLabel_Etc.Text = "          ";
+            // 
+            // toolStripStatusLabel8
+            // 
+            this.toolStripStatusLabel8.Name = "toolStripStatusLabel8";
+            this.toolStripStatusLabel8.Size = new System.Drawing.Size(408, 19);
+            this.toolStripStatusLabel8.Spring = true;
+            this.toolStripStatusLabel8.Text = " ";
+            // 
+            // button_Capture
+            // 
+            this.button_Capture.BackColor = System.Drawing.SystemColors.Control;
+            this.button_Capture.ImageIndex = 0;
+            this.button_Capture.ImageList = this.imageList1;
+            this.button_Capture.Location = new System.Drawing.Point(528, 7);
+            this.button_Capture.Name = "button_Capture";
+            this.button_Capture.Size = new System.Drawing.Size(38, 38);
+            this.button_Capture.TabIndex = 38;
+            this.button_Capture.UseVisualStyleBackColor = false;
+            this.button_Capture.Click += new System.EventHandler(this.button_Capture_Click);
+            // 
+            // button_Play
+            // 
+            this.button_Play.BackColor = System.Drawing.SystemColors.Control;
+            this.button_Play.ImageIndex = 3;
+            this.button_Play.ImageList = this.imageList1;
+            this.button_Play.Location = new System.Drawing.Point(606, 7);
+            this.button_Play.Name = "button_Play";
+            this.button_Play.Size = new System.Drawing.Size(38, 38);
+            this.button_Play.TabIndex = 40;
+            this.button_Play.UseVisualStyleBackColor = false;
+            this.button_Play.Click += new System.EventHandler(this.button_Play_Click);
+            // 
+            // button_Open2
+            // 
+            this.button_Open2.BackColor = System.Drawing.SystemColors.Control;
+            this.button_Open2.ImageIndex = 7;
+            this.button_Open2.ImageList = this.imageList1;
+            this.button_Open2.Location = new System.Drawing.Point(688, 7);
+            this.button_Open2.Name = "button_Open2";
+            this.button_Open2.Size = new System.Drawing.Size(38, 38);
+            this.button_Open2.TabIndex = 41;
+            this.button_Open2.UseVisualStyleBackColor = false;
+            this.button_Open2.Click += new System.EventHandler(this.button_Open2_Click);
+            // 
+            // Image2
+            // 
+            this.Image2.BackColor = System.Drawing.Color.White;
+            this.Image2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Image2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Image2.Location = new System.Drawing.Point(603, 62);
+            this.Image2.Name = "Image2";
+            this.Image2.Size = new System.Drawing.Size(160, 110);
+            this.Image2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Image2.TabIndex = 66;
+            this.Image2.TabStop = false;
+            this.Image2.Visible = false;
+            this.Image2.Click += new System.EventHandler(this.Image2_Click);
+            // 
+            // button_Face
+            // 
+            this.button_Face.BackColor = System.Drawing.SystemColors.Control;
+            this.button_Face.ImageIndex = 5;
+            this.button_Face.ImageList = this.imageList1;
+            this.button_Face.Location = new System.Drawing.Point(647, 7);
+            this.button_Face.Name = "button_Face";
+            this.button_Face.Size = new System.Drawing.Size(38, 38);
+            this.button_Face.TabIndex = 67;
+            this.button_Face.UseVisualStyleBackColor = false;
+            this.button_Face.Click += new System.EventHandler(this.button_Face_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "이미지/동영상|*.avi;*.mp4;*.bmp;*.jpg;*.png|모든 파일|*.*";
+            this.openFileDialog1.Title = " ";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 508);
+            this.Controls.Add(this.button_Face);
+            this.Controls.Add(this.Image2);
+            this.Controls.Add(this.button_Open2);
+            this.Controls.Add(this.button_Play);
+            this.Controls.Add(this.button_Capture);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button_Record);
+            this.Controls.Add(this.button_Close);
+            this.Controls.Add(this.Image1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OpenCV  Part4 Camera       by Induk Univ. 윤형태 (2019.07-2020.02.20)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Image1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Image2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenCvSharp.UserInterface.PictureBoxIpl Image1;
+        private System.Windows.Forms.Button button_Record;
+        private System.Windows.Forms.Button button_Close;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_width;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_Time;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_etcname;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_Etc;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel8;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_Name;
+        private System.Windows.Forms.Button button_Capture;
+        private System.Windows.Forms.Button button_Play;
+        private System.Windows.Forms.Button button_Open2;
+        private OpenCvSharp.UserInterface.PictureBoxIpl Image2;
+        private System.Windows.Forms.Button button_Face;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿//=====================================================================================
+// Digital Image Processing (OCV Part4  Camera)
+//
+// Yoon Hyung Tae (yht@induk.ac.kr)  2019.07 - 2020.02
+// Gamejigi (Computer Software, Induk University)
+//=====================================================================================
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OpenCvSharp;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;   // marshal 관련 선언
+
+using OpenCvSharp.Extensions;
+
+namespace OCV
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture video;
+
+        Mat src = new Mat();
+        Mat two = new Mat();
+        Mat dst = new Mat();
+
+        Boolean isRecording = false;    // 녹음 On/Off
+        Boolean isFace = false;           // 얼굴인식 On/Off
+
+        VideoWriter Record;                  // video 전용 class
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            
+            // Timer 설정
+            timer1.Interval = 33; // 30 프레임/초 (1000ms / 30fps)
+            timer1.Tick += new EventHandler(timer1_Tick);
+
+            // 웹캠 연결
+            video = new VideoCapture(0);
+            if (!video.IsOpened())
+            {
+                MessageBox.Show("웹캠을 열 수 없습니다!");
+                return;
+            }
+
+            // 타이머 시작
+            timer1.Start(); 
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*
+            // 웹캠에서 프레임 읽기
+            video.Read(src);
+
+            // 좌우 반전
+            Cv2.Flip(src, dst, FlipMode.Y);
+
+            // 이미지 출력
+            if (!dst.Empty())
+            {
+                Image1.Image = BitmapConverter.ToBitmap(dst);
+            }*/
+
+            if (isRecording)  // 녹화중인 경우
+            {
+                //Mat frame = new Mat(); // 프레임을 저장할 Mat 객체 생성
+                // 여기서 웹캠에서 프레임을 읽거나, 이미지 프레임을 생성하는 코드를 작성해야 합니다.
+                video.Read(src);
+
+                if (!src.Empty())
+                {
+                    Record.Write(src); // 영상 녹화
+                }
+            }
+
+            if (isFace) // 얼굴인식인 경우
+            {
+
+
+            }
+
+
+        }
+
+        // 폼 닫힐 때 웹캠 해제
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (video != null && video.IsOpened())
+            {
+                video.Release();
+            }
+        }
+
+        //------------------------------------------------------------------
+        // Button: Screen Capture
+        //------------------------------------------------------------------
+        private void button_Capture_Click(object sender, EventArgs e)
+        {
+            dst = Image1.ImageIpl;
+
+            if (!dst.Empty())
+            {
+                // 이미지 캡처 및 저장
+                string fileName = DateTime.Now.ToString("yyyy-MM-dd HHmmss") + ".png";
+                string filePath = @"C:\MyImageFoler\" + fileName; // 저장할 경로로 수정
+
+                Cv2.ImWrite(filePath, dst);
+
+                // 이미지 표시 - 새 창으로
+                Cv2.ImShow("캡처 이미지", dst);
+                Cv2.WaitKey(0); // 사용자가 키를 누를 때까지 대기
+
+                // 이미지 창 닫기
+                Cv2.DestroyWindow("캡처 이미지");
+            }
+            else
+            {
+                MessageBox.Show("이미지를 캡처할 수 없습니다.");
+            }
+        }
+        //------------------------------------------------------------------
+        // Button: Video Record Start
+        //------------------------------------------------------------------
+        private void button_Record_Click(object sender, EventArgs e)
+        {
+            isRecording = !isRecording;
+            if (isRecording)
+            {
+                button_Record.ImageIndex = 2;   // 버튼이미지 : ●
+
+                string fileName = "output.mp4"; // 저장할 파일명 지정 (확장자에 주의하세요)
+                int fourCC = VideoWriter.FourCC('X', 'V', 'I', 'D'); // 코덱 설정 (여기서는 XVID를 사용합니다)
+                double fps = 30.0; // 프레임 속도 설정 (여기서는 30fps)
+
+                Record = new VideoWriter(@"C:\MyImageFoler\" + fileName, fourCC, fps, new OpenCvSharp.Size(640, 480)); // 파일명, 코덱, FPS, 해상도
+                if (!Record.IsOpened())
+                {
+                    MessageBox.Show("녹화를 시작할 수 없습니다!");
+                    return;
+                }
+
+                isRecording = true;
+
+            }
+            else
+            {
+                button_Record.ImageIndex = 1;   // 버튼이미지 : ■
+
+                isRecording = false;
+                Record.Dispose();
+
+            }
+        }
+        //------------------------------------------------------------------
+        // Button: Video Play
+        //------------------------------------------------------------------
+        private void button_Play_Click(object sender, EventArgs e)
+        {
+            // OpenFileDialog를 이용하여 동영상 파일 선택
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "동영상 파일 (*.mp4;*.avi)|*.mp4;*.avi|모든 파일 (*.*)|*.*";
+            openFileDialog1.Title = "동영상 파일 선택";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // 선택한 동영상 파일명으로 VideoCapture 객체 생성
+                VideoCapture video1 = new VideoCapture(openFileDialog1.FileName);
+
+                if (!video1.IsOpened())
+                {
+                    MessageBox.Show("동영상을 열 수 없습니다.");
+                    return;
+                }
+
+                // PictureBox에 이미지 표시 준비
+                Image1.SizeMode = PictureBoxSizeMode.StretchImage; // 이미지가 PictureBox에 맞게 조절되도록 설정
+
+                // 동영상 재생
+                Mat frame = new Mat();
+                int delay = (int)Math.Round(1000 / video1.Fps);
+
+                while (true)
+                {
+                    video1.Read(frame); // 동영상에서 프레임 읽기
+
+                    if (frame.Empty())
+                    {
+                        break; // 더 이상 프레임이 없으면 반복문 종료
+                    }
+
+                    Image1.Image = BitmapConverter.ToBitmap(frame); // PictureBox에 프레임 표시
+
+                    int key = Cv2.WaitKey(delay);
+                    if (key == 27) // ESC 키를 누르면 동영상 재생 종료
+                    {
+                        break;
+                    }
+                }
+
+                video1.Dispose(); // 동영상 재생이 끝나면 VideoCapture 객체 해제
+
+
+            }
+        }
+        //------------------------------------------------------------------
+        // Button: Close
+        //------------------------------------------------------------------
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //------------------------------------------------------------------
+        // Button: Face
+        //------------------------------------------------------------------
+        private void button_Face_Click(object sender, EventArgs e)
+        {
+            isFace = ! isFace;
+            if (isFace)
+                button_Face.ImageIndex = 6;  // 버튼이미지: 얼굴+사각
+            else
+                button_Face.ImageIndex = 5;  // 버튼이미지: 얼굴
+        }
+        //------------------------------------------------------------------
+        // Button: Open Image2
+        //------------------------------------------------------------------
+        private void button_Open2_Click(object sender, EventArgs e)
+        {
+            Refresh();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String Image1_Filename = openFileDialog1.FileName;
+                string Document_Format = Image1_Filename.Substring(Image1_Filename.Length - 3, 3).ToUpper();
+                if (Document_Format == "BMP" || Document_Format == "JPG" || Document_Format == "PNG")
+                {
+                    src = Cv2.ImRead(openFileDialog1.FileName, ImreadModes.Color);
+                    Image1.ImageIpl = src;
+                    Image1.Visible = true;
+                }
+                else
+                    MessageBox.Show("JPG, PNG, BMP 만 사용할 수 있습니다.", "에러",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+        //------------------------------------------------------------------
+        private void Image2_Click(object sender, EventArgs e)
+        {
+            Image2.Visible = false;
+        }
+        //------------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
